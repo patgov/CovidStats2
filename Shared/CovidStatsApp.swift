f7ac793e9d7be8ac37cb259ddc830479d2c02e5d
Pat Govan <pat@govango.us>
2022-06-14 11:24:29 -0500

    Initial Commit

---
+++ Shared/CovidStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidStatsApp.swift
+//  Shared
+//
+//  Created by Pat Govan on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CovidStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
